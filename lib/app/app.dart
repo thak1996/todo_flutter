302b2353d91af09fca4ef1f56b5cc766cf81fc4f
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-07-21 12:53:58 -0300

    Remove theme management using Bloc; simplify app structure by eliminating ThemeCubit and related components

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_flutter/app/l10n/app_localizations.dart';
 import 'app.provider.dart';
 import 'app.router.dart';
 import 'core/theme/app.theme.dart';
-import 'core/theme/theme_cubit.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,24 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: AppProvider.providers,
-      child: BlocProvider(
-        create: (_) => ThemeCubit(storage: FlutterSecureStorage()),
-        child: BlocBuilder<ThemeCubit, ThemeState>(
-          builder: (context, state) {
-            return MaterialApp.router(
-              onGenerateTitle: (ctx) => AppLocalizations.of(ctx).titleApp,
-              localizationsDelegates: AppLocalizations.localizationsDelegates,
-              supportedLocales: AppLocalizations.supportedLocales,
-              darkTheme: AppTheme.darkTheme,
-              theme: AppTheme.lightTheme,
-              themeMode: state == ThemeState.light
-                  ? ThemeMode.light
-                  : ThemeMode.dark,
-              debugShowCheckedModeBanner: false,
-              routerConfig: appRouter,
-            );
-          },
-        ),
+      child: MaterialApp.router(
+        onGenerateTitle: (ctx) => AppLocalizations.of(ctx).titleApp,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        routerConfig: appRouter,
       ),
     );
   }
--- lib/app/app.provider.dart
+++ lib/app/app.provider.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:todo_flutter/app/page/home/home.controller.dart';
-import 'core/theme/theme_cubit.dart';
 import 'page/auth/login/login.controller.dart';
 
 class AppProvider {
   static List<SingleChildWidget> get providers => [
-    BlocProvider<ThemeCubit>(create: (_) => ThemeCubit()),
     BlocProvider<LoginController>(create: (_) => LoginController()),
     BlocProvider<HomeController>(create: (_) => HomeController()),
   ];
