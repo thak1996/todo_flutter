e27933f876ee70f4b145029a1d7df04b148963ac
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-07-24 11:09:58 -0300

    Refactor main function to initialize Firebase asynchronously before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_flutter/firebase_options.dart';
 import 'app/app.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
