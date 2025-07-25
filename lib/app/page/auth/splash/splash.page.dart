6fc7635ff3e8775b8f5729b271fcd2ee29324ae3
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-07-24 21:26:05 -0300

    Refactor SplashPage to use StatefulWidget and implement authentication redirection logic

--- lib/app/page/auth/splash/splash.page.dart
+++ lib/app/page/auth/splash/splash.page.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_flutter/app/core/routes/app.router.dart';
 import 'package:todo_flutter/app/l10n/app_localizations.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      if (authNotifier.isAuthenticated) {
+        context.go('/home');
+      } else {
+        context.go('/login');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
